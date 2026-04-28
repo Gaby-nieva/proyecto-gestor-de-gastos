d5bc93cfffd3fa250cf7487a00bc10133fb8deff
Gabriel Nieva <gabynieva7@gmail.com>
2026-04-28 15:56:19 -0300

    version final del gestor de gastos

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
-const formulario = document .getElementsByClassName ("formulario");
-const lista = document .getElementsByClassName ("lista-gastos");
-const totalspan = document .getElementsByClassName ("total");
+const formulario = document.querySelector(".formulario");
+const lista = document.querySelector(".lista-gastos");
+const totalSpan = document.querySelector(".total");
+const btnLimpiar = document.querySelector(".limpiar");
 
-let gastos =JSON.parse(localStorage.getItem("gastos")) || [];
+let gastos = JSON.parse(localStorage.getItem("gastos")) || [];
 
 formulario.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    const nombre = document.getElementsByClassName("nombre").value;
-    const monto = Number(document.getElementsByClassName("monto").value);
+    const nombre = document.querySelector(".nombre").value;
+    const monto = Number(document.querySelector(".monto").value);
+
+    if (nombre.trim() === "" || isNaN(monto) || monto <= 0) {
+        alert("Todos los campos son obligatorios y el monto debe ser mayor a 0");
+        return;
+    }
 
     const gasto = {
         id: Date.now(),
@@ -16,29 +22,43 @@ formulario.addEventListener("submit", function(e) {
         monto
     };
 
-    gastos.push("gasto");
+    gastos.push(gasto); 
     guardarStorage();
     mostrarGastos();
     formulario.reset();
-} );
+});
+
+btnLimpiar.addEventListener("click", () => {
+    const confirmar = confirm("¿Seguro que querés eliminar todos los gastos?");
+    if (confirmar) {
+        gastos = [];
+        guardarStorage();
+        mostrarGastos();
+    }
+});
 
 function mostrarGastos() {
     lista.innerHTML = "";
+
+    if (gastos.length === 0) {
+        lista.innerHTML = "<p>No hay gastos aún</p>";
+        totalSpan.textContent = 0;
+        return;
+    }
 
     gastos.forEach(gasto => {
         const li = document.createElement("li");
 
         li.innerHTML = `
             ${gasto.nombre} - $${gasto.monto}
-            <button onClick="eliminarGasto(${gasto.classname})">X</button>`;
+            <button onclick="eliminarGasto(${gasto.id})">X</button>
+        `;
 
-            lista.appenChild(li); 
-        
+        lista.appendChild(li); 
     });
 
     calcularTotal();
 }
-
 
 function eliminarGasto(id) {
     gastos = gastos.filter(g => g.id !== id);
